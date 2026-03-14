3b454de18e0b7e6d17066044d07a4c5bd48cd217
bloghaynhat <22683091.duc@student.iuh.edu.vn>
2026-03-14 14:08:04 +0700

    Tuan02

---
+++ Tuan02/README.md
@@ -0,0 +1,4 @@
+# 1. Facotry Method
+![img.png](src/img.png)
+# 2. Abstract Factory
+![img.png](img.png)
--- Tuan02/src/main/java/iuh/fit/se/AbstractFactory.java
+++ Tuan02/src/main/java/iuh/fit/se/AbstractFactory.java
@@ -71,5 +71,74 @@ class WildDog implements Dog {
         return "wild dog";
     }
 }
+
+class PetAnimalFactory extends AnimalFactory {
+    public PetAnimalFactory() {
+        System.out.println("You opt for a pet animal factory.\n");
+    }
+    @Override
+    public Tiger createTiger(String color) {
+        return new PetTiger(color);
+    }
+    @Override
+    public Dog createDog(String color) {
+        return new PetDog(color);
+    }
+}
+
+class PetTiger implements Tiger {
+    public PetTiger(String color) {
+        System.out.println("A pet tiger with " + color + "color is created.");
+
+    }
+    public void aboutMe() {
+        System.out.println("The " + this + " says: Halum. I play in an animal circus.");
+
+    }
+    public void inviteDog(Dog dog) {
+        System.out.println("The " + this + " says: I saw a" + dog + " in my town.");
+
+    }
+    @Override
+    public String toString() {
+        return "pet tiger";
+    }
+}
+class PetDog implements Dog {
+    public PetDog(String color) {
+        System.out.println("A pet dog with " + color + "color is created.");
+
+    }
+    @Override
+    public void displayMe() {
+        System.out.println("The " + this + " says: Bow-Wow. I prefer to stay at home.");
+
+    }
+    @Override
+    public String toString() {
+        return "pet dog";
+    }
+}
 public class AbstractFactory {
+    public static void main(String[] args) {
+        System.out.println("***Abstract Factory Pattern Demo.***\n");
+        AnimalFactory animalFactory;
+// Making a wild dog and wild tiger through
+
+// WildAnimalFactory
+        animalFactory = new WildAnimalFactory();
+        Dog dog = animalFactory.createDog("white");
+        Tiger tiger = animalFactory.createTiger("golden and cinnamon");
+        dog.displayMe();
+        tiger.aboutMe();
+        tiger.inviteDog(dog);
+        System.out.println("\n************\n");
+// Making a pet dog and pet tiger through
+// PetAnimalFactory now.
+        animalFactory = new PetAnimalFactory();
+        dog = animalFactory.createDog("black");
+        tiger = animalFactory.createTiger("yellow");
+        dog.displayMe();
+        tiger.aboutMe();
+        tiger.inviteDog(dog);}
 }
